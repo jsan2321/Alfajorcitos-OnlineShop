bb794746217acd0eddd9ecc8a81b90bae5789e51
jsan2321 <jsantiago.c@hotmail.com>
2024-01-28 13:35:19 -0500

    initial commit for the alfores shop

---
+++ admin/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+include '../components/connect.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Admin - Dashboard</title>
+  </head>
+  <body>
+    <?php include '../components/admin_header.php'?>
+
+
+  </body>
+</html>
