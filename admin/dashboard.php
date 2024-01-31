1275b9dd668dd8f72e179559aca865a9b61b79ff
jsan2321 <jsantiago.c@hotmail.com>
2024-01-31 11:47:57 -0500

    added logo and icons icons in admin_header file

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,11 +9,20 @@ include '../components/connect.php';
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- fetches the Boxicons stylesheet from the CDN (Content Delivery Network). Include and load the Boxicons library to display icons.-->
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+      <link rel="stylesheet" type="text/css" href=".../css/admin_style.css?v=<?php echo time(); ?>" > <!-- appending a query parameter to the URL, which includes the current timestamp -->
       <title>Admin - Dashboard</title>
   </head>
   <body>
     <?php include '../components/admin_header.php'?>
 
+    <!-- sweetalert cdn link -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- custom js link  -->
+    <script type="text/javascript" src=".../js/admin_scripts.js"></script>
+    <!-- alert -->
+    <?php include '.../components/alert.php' ?>
 
   </body>
 </html>
--- components/admin_header.php
+++ components/admin_header.php
@@ -1,1 +1,44 @@
-<h1>Admin Header</h1>
+<?php
+
+  error_reporting(E_ALL); // reports and logs all errors
+  ini_set('display_errors', '1'); //  display the errors directly on the web page
+
+?>
+
+<header>
+
+  <div class="logo">
+    <img src="../image/logo.png" alt="alfajorcitos logo" width="100">  <!-- '..' indicates moving up one level in the directory structure. This is a relative path. -->
+  </div>
+
+  <div class="right">
+    <div class='bx bxs-user' id="user-btn"></div>
+    <div class="toggle-btn"><i class="bx bx-menu"></i></div>
+  </div>
+
+
+</header>
+
+<div class="sidebar">
+
+
+  <h5>menu</h5>
+  <div class="navbar">
+    <ul>
+        <li><a href="dashboard.php"><i class="bx bxs-home-smile"></i>dashboard</a></li>
+        <li><a href="add_product.php"><i class="bx bxs-shopping-bags"></i>add products</a></li>
+        <li><a href="view_products.php"><i class="bx bxs-food-menu"></i>view products</a></li>
+        <li><a href="user_accounts.php"><i class="bx bxs-user-detail"></i>accounts</a></li>
+        <li><a href="../components/admin_logout.php" onclick="return confirm('logout from this website');" ><i class="bx bxs-log-out"></i>logout</a></li>
+    </ul>
+  </div>
+  <h5>find us</h5>
+  <div class="social-links">
+      <i class='bx bxl-facebook'></i>
+      <i class="bx bxl-instagram-alt"></i>
+      <i class="bx bxl-linkedin"></i>
+      <i class="bx bxl-twitter"></i>
+      <i class="bx bxl-pinterest-alt"></i>
+  </div>
+    
+</div>
