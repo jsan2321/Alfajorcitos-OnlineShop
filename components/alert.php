c3f6f97386c1e1ef080f7a9902fd19008516d760
jsan2321 <jsantiago.c@hotmail.com>
2024-02-23 09:54:50 -0500

    fixed alert messages

--- components/alert.php
+++ components/alert.php
@@ -2,25 +2,25 @@
 
     if(isset($success_msg)) {
         foreach($success_msg as $success) {
-            echo '<script> swal("'.$success.'", "", "success"); </script>';
+            echo '<script> Swal.fire("'.$success.'", "", "success"); </script>';
         }
     }
 
     if(isset($warning_msg)) {
         foreach($warning_msg as $warning) {
-            echo '<script> swal("'.$warning.'", "", "warning"); </script>';
+            echo '<script> Swal.fire("'.$warning.'", "", "warning"); </script>';
         }
     }
 
     if(isset($info_msg)) {
         foreach($info_msg as $info) {
-            echo '<script> swal("'.$info.'", "", "info"); </script>';
+            echo '<script> Swal.fire("'.$info.'", "", "info"); </script>';
         }
     }
 
     if(isset($error_msg)) {
         foreach($error_msg as $error) {
-            echo '<script> swal("'.$error.'", "", "error"); </script>';
+            echo '<script> Swal.fire("'.$error.'", "", "error"); </script>';
         }
     }
 ?>
