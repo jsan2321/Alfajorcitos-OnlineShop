3c5cdf82c98aa2544c992b3a8572fafd2564ebb3
jsan2321 <jsantiago.c@hotmail.com>
2024-04-05 21:22:50 -0500

    added use account  php page

--- admin/user_account.php
+++ admin/user_account.php
@@ -44,47 +44,47 @@
       <!-- fetches the Boxicons stylesheet from the CDN (Content Delivery Network). Include and load the Boxicons library to display icons.-->
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
       <link rel="stylesheet" type="text/css" href="../css/admin_style.css?v=<?php echo time(); ?>" > <!-- appending a query parameter to the URL, which includes the current timestamp -->
-      <title>Alfajorcitos - alfajores web site user messages page</title>
+      <title>Alfajorcitos - alfajores web site registered user page</title>
   </head>
   <body>
 
     <?php include '../components/admin_header.php'?>
     <div class="banner">
       <div class="detail">
-        <h1>user messages</h1>
+        <h1>registered user</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi tenetur aut repudiandae libero dolore, delectus error sit repellendus assumenda possimus vitae unde quis quas sapiente reiciendis earum in dignissimos distinctio.</p>
-        <span><a href="dashboard.php">admin</a><i class="bx bx-right-arrow-alt"></i>user message</span>
+        <span><a href="dashboard.php">admin</a><i class="bx bx-right-arrow-alt"></i>registered user</span>
       </div>
     </div>
 
-    <section class="message-container">
+    <section class="user-container">
         <div class="heading">
-            <h1>user message's</h1>
+            <h1>registered user</h1>
             <img src="../image/separator.png">
         </div>
         <div class="box-container"> 
             <?php
-                $select_message = $conn->prepare("SELECT * FROM `message`");
-                $select_message->execute();
+                $select_users = $conn->prepare("SELECT * FROM `users`");
+                $select_users->execute();
 
-                if( $select_message->rowCount() > 0) {
-                    while($fetch_message = $select_message->fetch(PDO::FETCH_ASSOC)) {
+                if( $select_users->rowCount() > 0) {
+                    while($fetch_users = $select_users->fetch(PDO::FETCH_ASSOC)) {
+                        $user_id = $fetch_users['id'];
             ?>
             <div class="box">
-                <h3 class="name"><?= $fetch_message['name']; ?></h3>
-                <h4><?= $fetch_message['subject']; ?></h4>
-                <p><?= $fetch_message['message']; ?></p>
-                <form action="" method="post" >
-                    <input type="hidden" name="delete_id" value="<? $fetch_message['id']; ?>" >
-                    <button type="submit" name="delete" class="btn" onclick="return confirm('delete this message');" >delete message</button>
-                </form>
+                <img src="../uploaded_files/<?= $fetch_users['image']; ?>" >
+                <div class="detail">
+                    <p>user id : <span> <?= $user_id; ?> </span> </p>
+                    <p>user name : <span> <?= $fetch_users['name']; ?> </span> </p>
+                    <p>user email : <span> <?= $fetch_users['email']; ?> </span> </p>
+                </div>
             </div>
             <?php
                     }
                 } else {
                     echo '
                         <div class="empty" style="margin: 2rem auto;" >
-                            <p>no message</p>
+                            <p>no user registered yet</p>
                         </div>
                    ';
                 }
