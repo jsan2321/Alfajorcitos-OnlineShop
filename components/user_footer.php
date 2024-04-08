e3990d2fdb2df79eeae165b8a74ebee543e224a3
jsan2321 <jsantiago.c@hotmail.com>
2024-04-08 13:15:00 -0500

    added user footer for the home page

---
+++ components/user_footer.php
@@ -0,0 +1,81 @@
+<div class="newsletter">
+    <div class="box-detail">
+        <h1>subscribe to our newsletter</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat dignissimos soluta placeat quas? Accusantium incidunt assumenda in architecto voluptatibus impedit ipsa, reprehenderit repellendus mollitia! Minima quisquam dolore recusandae qui a.</p>
+        <div class="input-field">
+            <input type="email" name="enter your mail..." class="box"><br>
+            <button class="btn">subscribe</button>
+        </div>
+        <div class="icons">
+            <i class='bx bxl-facebook'></i>
+            <i class="bx bxl-instagram-alt"></i>
+            <i class="bx bxl-linkedin"></i>
+            <i class="bx bxl-twitter"></i>
+            <i class="bx bxl-pinterest-alt"></i>
+        </div>
+    </div>
+</div>
+<div class="client">
+    <div class="img-box">
+        <img src="image/client.jpg">
+    </div>
+    <div class="img-box">
+        <img src="image/client0.jpg">
+    </div>
+    <div class="img-box">
+        <img src="image/client1.jpg">
+    </div>
+    <div class="img-box">
+        <img src="image/client2.jpg">
+    </div>
+    <div class="img-box">
+        <img src="image/client.jpg">
+    </div>
+</div>
+<footer>
+    <div class="content">
+        <div class="box">
+            <img src="image/logo.png">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae ab quas ratione, asperiores amet enim harum dicta, ut deserunt expedita explicabo nobis adipisci sit! Similique quibusdam dolore distinctio eveniet.</p>
+            <a href="contact.php" class="btn">contact with us</a>
+        </div>
+        <div class="box">
+            <h3>my account</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>my account</a>
+            <a href=""><i class="bx bx-chevron-right"></i>order history</a>
+            <a href=""><i class="bx bx-chevron-right"></i>wishlist</a>
+            <a href=""><i class="bx bx-chevron-right"></i>newsletter</a>
+        </div>
+        <div class="box">
+            <h3>information</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>about us</a>
+            <a href=""><i class="bx bx-chevron-right"></i>delivery information</a>
+            <a href=""><i class="bx bx-chevron-right"></i>privacy policy</a>
+            <a href=""><i class="bx bx-chevron-right"></i>terms & conditions</a>
+        </div>
+        <div class="box">
+            <h3>extras</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>brands</a>
+            <a href=""><i class="bx bx-chevron-right"></i>gift certificates</a>
+            <a href=""><i class="bx bx-chevron-right"></i>affiliate</a>
+            <a href=""><i class="bx bx-chevron-right"></i>specials</a>
+        </div>
+        <div class="box">
+            <h3>contact us</h3>
+            <p><i class="bx bxs-phone"></i>+91-2233445566</p>
+            <p><i class="bx bxs-envelope"></i>joseantonio@gmail.com</p>
+            <p><i class="bx bxs-location-plus"></i>South America, Peru</p>
+            <div class="icons">
+                <i class="bx bxl-facebook"></i>
+                <i class="bx bxl-instagram-alt"></i>
+                <i class="bx bxl-linkedin"></i>
+                <i class="bx bxl-twitter"></i>
+                <!-- <i class="bx bxl-pinterest-alt"></i> -->
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <p>copyright @ 2024 code with antonio. All Rights Reserved</p>
+        <a href="admin/register.php">become a seller</a>
+    </div>
+</footer>
